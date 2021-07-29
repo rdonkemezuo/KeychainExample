f0a00e77c844cd4ebbb55bd14d127047c4c47867
Raymond Donkemezuo <rdonkemezuo@alpine-lis.com>
2021-07-29 10:47:32 -0700

    Initial Commit

---
+++ KeychainExample/KeychainExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainExampleApp.swift
+//  KeychainExample
+//
+//  Created by Raymond Donkemezuo on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct KeychainExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
